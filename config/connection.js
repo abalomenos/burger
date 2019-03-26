bd516fa7b6f38fa89bcfe2614662fb2711a25548
abalomenos <arisbalomenos@gmail.com>
2019-03-25 17:24:02 -0700

    Heroku Update

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 
-// For MySQL Connection info.
+// For MySQL Local Connection info.
 require('dotenv').config();
+
+// File to process .env for MySQL Local Connection Info.
+var keys = require("./keys.js");
 
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-// File to process .env for MySQL Connection Info.
-var keys = require("./keys.js");
+var connection;
 
 // MySQL Database Connection.
-var connection = mysql.createConnection({
-  host: keys.mysql.host,
-  port: keys.mysql.port,
-  user: keys.mysql.user,
-  password: keys.mysql.password,
-  database: keys.mysql.database
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {  
+  connection = mysql.createConnection({
+    host: keys.mysql.host,
+    port: keys.mysql.port,
+    user: keys.mysql.user,
+    password: keys.mysql.password,
+    database: keys.mysql.database
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
